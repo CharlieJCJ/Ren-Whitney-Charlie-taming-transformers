0e5c794e58a130afefbd72a8a987eefb920fc76f
CharlieJCJ <charliechengjieji@berkeley.edu>
2022-10-21 01:22:52 -0700

    contrastive loss added, needed test

--- taming/data/base.py
+++ taming/data/base.py
@@ -4,6 +4,11 @@ import albumentations
 from PIL import Image
 from torch.utils.data import Dataset, ConcatDataset
 
+# Packages used in simclr
+from torchvision.transforms import transforms
+import torch.nn as nn
+import torch
+
 
 class ConcatDatasetWithIndex(ConcatDataset):
     """Modified from original pytorch code to return dataset idx"""
@@ -19,7 +24,8 @@ class ConcatDatasetWithIndex(ConcatDataset):
             sample_idx = idx - self.cumulative_sizes[dataset_idx - 1]
         return self.datasets[dataset_idx][sample_idx], dataset_idx
 
-
+# TODO: add constrastive loss for views
+# ImagePaths class overwrites base torch Dataset class, so need to implement __len__ and __getitem__
 class ImagePaths(Dataset):
     def __init__(self, paths, size=None, random_crop=False, labels=None):
         self.size = size
@@ -42,22 +48,87 @@ class ImagePaths(Dataset):
     def __len__(self):
         return self._length
 
+    @staticmethod # Simclr transformation on VIEWS images
+    def get_simclr_pipeline_transform(size, s=1):
+        """Return a set of data augmentation transformations as described in the SimCLR paper."""
+        color_jitter = transforms.ColorJitter(0.8 * s, 0.8 * s, 0.8 * s, 0.2 * s)
+        
+        # Reshape to size x size (no matter what)
+        data_transforms = transforms.Compose([transforms.Resize(size=(size, size)),
+                                              transforms.RandomResizedCrop(size=size),
+                                              transforms.RandomHorizontalFlip(),
+                                              transforms.RandomApply([color_jitter], p=0.8),
+                                              transforms.RandomGrayscale(p=0.2),
+                                              GaussianBlur(kernel_size=int(0.1 * size)),
+                                              transforms.ToTensor()])
+
+        return data_transforms
+
+    # TODO: add constrastive loss for views (it calls `self.preprocess_image`)
     def preprocess_image(self, image_path):
         image = Image.open(image_path)
         if not image.mode == "RGB":
             image = image.convert("RGB")
         image = np.array(image).astype(np.uint8)
         image = self.preprocessor(image=image)["image"]
-        image = (image/127.5 - 1.0).astype(np.float32)
-        return image
 
+        # Add simclr VIEWS data
+        [view1, view2] = [self.get_simclr_pipeline_transform(size=self.size)(image) for i in range(2)]
+        image = (image/127.5 - 1.0).astype(np.float32)
+        [view1, view2] = [(view/127.5 - 1.0).astype(np.float32) for view in [view1, view2]]
+        return (image, view1, view2)
+    
+
+    
+    # Allow outside using object with indexing, e.g. `dataset[0]`, preprocess_image will be called in getitem
     def __getitem__(self, i):
         example = dict()
-        example["image"] = self.preprocess_image(self.labels["file_path_"][i])
+        example["image"] = self.preprocess_image(self.labels["file_path_"][i]) # now example["image"] is a tuple of 3 images
         for k in self.labels:
             example[k] = self.labels[k][i]
         return example
 
+# Added from Simclr `helper class`
+class GaussianBlur(object):
+    """blur a single image on CPU"""
+    def __init__(self, kernel_size):
+        radias = kernel_size // 2
+        kernel_size = radias * 2 + 1
+        self.blur_h = nn.Conv2d(3, 3, kernel_size=(kernel_size, 1),
+                                stride=1, padding=0, bias=False, groups=3)
+        self.blur_v = nn.Conv2d(3, 3, kernel_size=(1, kernel_size),
+                                stride=1, padding=0, bias=False, groups=3)
+        self.k = kernel_size
+        self.r = radias
+
+        self.blur = nn.Sequential(
+            nn.ReflectionPad2d(radias),
+            self.blur_h,
+            self.blur_v
+        )
+
+        self.pil_to_tensor = transforms.ToTensor()
+        self.tensor_to_pil = transforms.ToPILImage()
+
+    def __call__(self, img):
+        img = self.pil_to_tensor(img).unsqueeze(0)
+
+        sigma = np.random.uniform(0.1, 2.0)
+        x = np.arange(-self.r, self.r + 1)
+        x = np.exp(-np.power(x, 2) / (2 * sigma * sigma))
+        x = x / x.sum()
+        x = torch.from_numpy(x).view(1, -1).repeat(3, 1)
+
+        self.blur_h.weight.data.copy_(x.view(3, 1, self.k, 1))
+        self.blur_v.weight.data.copy_(x.view(3, 1, 1, self.k))
+
+        with torch.no_grad():
+            img = self.blur(img)
+            img = img.squeeze()
+
+        img = self.tensor_to_pil(img)
+
+        return img
 
 class NumpyPaths(ImagePaths):
     def preprocess_image(self, image_path):
