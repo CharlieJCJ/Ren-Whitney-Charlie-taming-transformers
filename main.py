78b04637ba0b6f6565f02c764782b1cb31d4751d
CharlieJCJ <charliechengjieji@berkeley.edu>
2022-10-23 13:33:29 -0700

    A

--- main.py
+++ main.py
@@ -439,7 +439,7 @@ if __name__ == "__main__":
         trainer_config["distributed_backend"] = "ddp"
 
         if not "gpus" in trainer_config:
-            trainer_config["gpus"] = "0,1"
+            trainer_config["gpus"] = "0,1" # default to 2 gpus
 
         for k in nondefault_trainer_args(opt):
             trainer_config[k] = getattr(opt, k)
